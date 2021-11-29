e08df9f11d2239a1b8ee129ef304dcee621d7eb9
Natalie Somersall <some-natalie@github.com>
2021-11-29 14:21:49 -0700

    move shell commands into yml file

--- tip-picker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Pick a random tip by key
-tipname=$(jq -r 'keys[]' tips.json | shuf | head -n 1)
-
-# Print that key's value as the tip
-tip=$(jq -r ."\"$tipname\"" tips.json)
-echo "::set-output name=tip::$(echo $tip)"
